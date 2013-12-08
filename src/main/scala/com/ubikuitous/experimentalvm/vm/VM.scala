0f307f6c5c7ca754de756ff89b3390ec8e75c7dc
Steven Obua <steven.obua@gmail.com>
2013-12-08 19:43:05 +0100

    Stack can now contain BOTH references and (small enough) integers.

--- src/main/scala/com/ubikuitous/experimentalvm/vm/VM.scala
+++ src/main/scala/com/ubikuitous/experimentalvm/vm/VM.scala
@@ -1,16 +1,18 @@
 package com.ubikuitous.experimentalvm.vm
 
-trait Reference 
-
 trait CodePointer 
+
+sealed trait StackElement
+
+trait Reference extends StackElement
 
 sealed abstract class Value 
 object Value {
+  case class INT(value : Long) extends Value with StackElement
   case class INTEGER(value : BigInt) extends Value
   case class CLOSURE(cp : CodePointer, gp : Reference) extends Value
   case class FUNCTION(cp : CodePointer, ap : Reference, gp : Reference) extends Value
-  case class VECTOR(elements: Array[Reference]) extends Value
-  case class SYMBOL(name : String) extends Value
+  case class VECTOR(elements: Array[StackElement]) extends Value
 }
 
 trait Heap {
@@ -23,15 +25,15 @@ trait Stack {
   var SP : Long
   var FP : Long 
   
-  def get(sp : Long) : Reference
-  def set(sp : Long, r : Reference)
+  def get(sp : Long) : StackElement
+  def set(sp : Long, e : StackElement)
     
-  def push(r : Reference) { 
+  def push(e : StackElement) { 
     SP = SP + 1
-    set(SP, r)
+    set(SP, e)
   }
   
-  def pop() : Reference = {
+  def pop() : StackElement = {
     val r = get(SP)
     SP = SP - 1
     r
@@ -54,30 +56,159 @@ trait ProgramStore {
 
 }
 
-sealed abstract class Instruction 
+sealed abstract class Instruction {
+  def execute(vm : RunVM) : Boolean 
+}
+
 object Instruction {
-  case object HALT extends Instruction
-  case object CRASH extends Instruction
-  case class LOADI(i : Value.INTEGER) extends Instruction
-  case class LOADS(s : Value.SYMBOL) extends Instruction
-  case object NEG extends Instruction
-  case object ADD extends Instruction 
-  case object SUB extends Instruction
-  case object MUL extends Instruction
-  case object DIV extends Instruction
-  case object MOD extends Instruction
-  case object AND extends Instruction
-  case object OR extends Instruction
-  case object NOT extends Instruction
-  case object EQ extends Instruction
-  case object NEQ extends Instruction
-  case object LE extends Instruction
-  case object LEQ extends Instruction
-  case object GR extends Instruction
-  case object GEQ extends Instruction
-  case class JUMP(cp : CodePointer) extends Instruction
-  case class JUMPZ(cp : CodePointer) extends Instruction
-  case class JUMPI(cp : CodePointer) extends Instruction
+  
+  case object HALT extends Instruction {
+    def execute(vm : RunVM) : Boolean = false
+  }
+  
+  case object CRASH extends Instruction {
+    def execute(vm : RunVM) : Boolean = vm.crash()
+  }
+  
+  sealed case class LOADINT(i : Value.INT) extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrLOADINT(i)
+      true
+    }
+  }
+  
+  sealed case class LOADINTEGER(i : Value.INTEGER) extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrLOADINTEGER(i)
+      true
+    }    
+  }
+    
+  case object NEG extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrNEG()
+      true
+    }            
+  }
+  
+  case object ADD extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrADD()
+      true
+    }               
+  } 
+  
+  case object SUB extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrSUB()
+      true
+    }               
+  } 
+  
+  case object MUL extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrMUL()
+      true
+    }               
+  } 
+  
+  case object DIV extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrDIV()
+      true
+    }               
+  } 
+  
+  case object MOD extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrMOD()
+      true
+    }               
+  } 
+  
+  case object NOT extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrNOT()
+      true
+    }               
+  } 
+
+  case object AND extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrAND()
+      true
+    }               
+  } 
+  
+  case object OR extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrOR()
+      true
+    }               
+  } 
+    
+  case object EQ extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrEQ()
+      true
+    }               
+  } 
+  
+  case object NEQ extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrNEQ()
+      true
+    }               
+  } 
+  
+  case object LE extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrLE()
+      true
+    }               
+  } 
+  
+  case object LEQ extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrLEQ()
+      true
+    }               
+  } 
+  
+  case object GR extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrGR()
+      true
+    }               
+  } 
+  
+  case object GEQ extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrGEQ()
+      true
+    }               
+  } 
+  
+  case class JUMP(cp : CodePointer) extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrJUMP(cp)
+      true
+    }               
+  } 
+  
+  case class JUMPZ(cp : CodePointer) extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrJUMPZ(cp)
+      true
+    }               
+  } 
+  
+  case class JUMPI(cp : CodePointer) extends Instruction {
+    def execute(vm : RunVM) : Boolean = {
+      vm.instrJUMPI(cp)
+      true
+    }               
+  } 
 }
 
 trait VM {
@@ -86,16 +217,27 @@ trait VM {
   val programStore : ProgramStore
   var GP : Reference  
   
-  def resolve(delta : Long) : Value = 
-    heap.resolve(stack.get(stack.SP - delta))
+  final def resolve(delta : Long) : Value = {
+    stack.get(stack.SP - delta) match {
+      case e : Value.INT => e
+      case r : Reference => heap.resolve(r)
+    }
+  }
     
-  def pop() : Value = {
-    heap.resolve(stack.pop())
+  final def pop() : Value = {
+    stack.pop() match {
+      case e : Value.INT => e
+      case r : Reference => heap.resolve(r)
+    }
   }
-  
-  def push(value : Value) = {
-    stack.push(heap.alloc(value))
+    
+  final def push(value : Value) = {
+    value match {
+      case i : Value.INT => stack.push(i)
+      case _ => stack.push(heap.alloc(value))
+    }
   }
+
 }
 
 trait RunVM extends VM {
@@ -104,110 +246,141 @@ trait RunVM extends VM {
     var instr : Instruction = null
     do {
       instr = programStore.read()
-    } while (execute(instr))
+    } while (instr.execute(this))
   }
 
-  private def crash() {
+  final def crash[T]() : T = {
     throw new RuntimeException("VM crashed")
-  }
-
-  private def execute(instruction : Instruction) : Boolean = {
-    import Instruction._
-    instruction match {
-      case HALT => return false
-      case CRASH => crash()
-      case LOADI(i) => instrLOADI(i)
-      case LOADS(s) => instrLOADS(s)
-      case NEG => instrNEG()
-      case ADD => instrADD()
-      case SUB => instrSUB()
-      case MUL => instrMUL()
-      case DIV => instrDIV()
-      case MOD => instrMOD()
-      //case AND => instrAND()
-      //case OR => instrOR()
-      case JUMP(cp) => instrJUMP(cp)
-      case JUMPZ(cp) => instrJUMPZ(cp)
-      case JUMPI(cp) => instrJUMPI(cp)
-    }
-    return true
   }
   
   import Value._
+    
+  private def pushi(i : BigInt) {
+    if (i.isValidLong) 
+      push(INT(i.toLong))
+    else
+      push(INTEGER(i))
+  }
   
-  private def instrNEG() {
+  private def popi() : BigInt = {
     pop() match {
-      case INTEGER(x) => push(new INTEGER(-x))
-      case _ => crash()     
+      case INT(x) => x
+      case INTEGER(x) => x
+      case _ => crash()
     }
   }
   
-  private def instrADD() {
-    (pop(), pop()) match {
-      case (INTEGER(x), INTEGER(y)) => push(new INTEGER(x + y))
+  private def popb() : Boolean = {
+    pop() match {
+      case INT(x) => x != 0
+      case INTEGER(x) => x != 0
       case _ => crash()
-    }  
+    }
   }
   
-  private def instrSUB() {
-    (pop(), pop()) match {
-      case (INTEGER(x), INTEGER(y)) => push(new INTEGER(x - y))
-      case _ => crash()
-    }      
+  private def pushb(b : Boolean) = {
+    push(INT(if (b) 1 else 0))
+  }
+  
+  final def instrNEG() {
+    pushi(-popi())
+  }
+  
+  final def instrADD() {
+    pushi(popi() + popi())
+  }
+  
+  final def instrSUB() {
+    pushi(popi() - popi())
   }
 
-  private def instrMUL() {
-    (pop(), pop()) match {
-      case (INTEGER(x), INTEGER(y)) => push(new INTEGER(x * y))
-      case _ => crash()
-    }      
+  final def instrMUL() {
+    pushi(popi() * popi())
   }
   
-  private def instrDIV() {
-    (pop(), pop()) match {
-      case (INTEGER(x), INTEGER(y)) => 
-        if (y == 0) crash() else push(new INTEGER(x / y))
-      case _ => crash()
-    }      
+  final def instrDIV() {
+    val x = popi()
+    val y = popi()
+    if (y == 0) crash() else pushi(x / y)
   }
 
-  private def instrMOD() {
-    (pop(), pop()) match {
-      case (INTEGER(x), INTEGER(y)) => 
-        if (y == 0) crash() else push(new INTEGER(x % y))
-      case _ => crash()
-    }      
+  final def instrMOD() {
+    val x = popi()
+    val y = popi()
+    if (y == 0) crash() else pushi(x % y)
   }
   
-  private def instrLOADI(i : INTEGER) {
+  final def instrNOT() {
+    pushb(!popb())
+  }
+  
+  final def instrAND() {
+    val u = popb()
+    val v = popb()
+    pushb(u && v)
+  }
+  
+  final def instrOR() {
+    val u = popb()
+    val v = popb()
+    pushb(u || v)
+  }  
+      
+  final def instrLOADINT(i : INT) {
     push(i)
   }
 
-  private def instrLOADS(s : SYMBOL) {
-    push(s)
+  final def instrLOADINTEGER(i : INTEGER) {
+    pushi(i.value)
   }
   
-  private def instrJUMP(cp : CodePointer) {
+  final def instrJUMP(cp : CodePointer) {
     programStore.goto(cp)
   }
   
-  private def instrJUMPZ(cp : CodePointer) {
-    pop() match {
-      case INTEGER(x) => 
-        if (x == 0) programStore.goto(cp)
-      case _ => crash()
-    }
+  final def instrJUMPZ(cp : CodePointer) {
+    if (popi() == 0) programStore.goto(cp)
   }
   
-  private def instrJUMPI(cp : CodePointer) {
-    pop() match {
-      case INTEGER(x) =>
-        if (x.isValidLong) programStore.goto(cp, x.toLong)
-        else crash()
-      case _ => crash()
-    }
+  final def instrJUMPI(cp : CodePointer) {
+    val x = popi()
+    if (x.isValidLong) programStore.goto(cp, x.toLong)
+    else crash()
   }
   
+  final def instrEQ() {
+    pushb(popi() == popi())
+  }
+
+  final def instrNEQ() {
+    pushb(popi() != popi())
+  }
+  
+  final def instrLE() {
+    pushb(popi() < popi())
+  }
+
+  final def instrLEQ() {
+    pushb(popi() <= popi())
+  }
+
+  final def instrGR() {
+    pushb(popi() > popi())
+  }
+  
+  final def instrGEQ() {
+    pushb(popi() >= popi())
+  }
+
+}
+
+object Test {
+  def main(args : Array[String]) {
+    val x : BigInt = 0
+    val y : Int = 0
+    val t = (x == y)
+    println("test: " + t)
+  }
 }
 
 
